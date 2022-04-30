de324e88d420d0864078a5bad274736af49d34c4
Stuart Green <github.47a8z@aleeas.com>
2022-04-30 11:13:28 +0100

    feat: add cors middleware

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { Application } from "express";
+import cors from "cors";
 
 import apiRouter from "./routes/apiRouter";
 import errorHandler from "./middleware/errorHandler";
 
 const app: Application = express();
 
+app.use(cors());
 app.use(express.json());
 app.use("/api", apiRouter);
 app.use(errorHandler);
