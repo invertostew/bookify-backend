82bfbfe6b95a80934a5c64467da4f12a3165888d
Stuart Green <github.47a8z@aleeas.com>
2022-05-07 12:18:04 +0100

    refactor: return bookings in the format react scheduler needs

--- src/models/CalendarStore.ts
+++ src/models/CalendarStore.ts
@@ -224,7 +224,9 @@ export class CalendarStore {
       const connection = await pool.connect();
       const sql = `
         SELECT
-          *
+          booking as startDate,
+          (SELECT booking + (duration ||' minutes')::interval as endDate),
+          services.title
         FROM
           bookings
         JOIN
