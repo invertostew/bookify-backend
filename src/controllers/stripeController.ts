67b40afffa49cd864a6c30a02660f55ac8c70464
Stuart Green <github.47a8z@aleeas.com>
2022-05-09 02:05:50 +0100

    feat: stripe webhook set up watching for checkout complete event

--- src/controllers/stripeController.ts
+++ src/controllers/stripeController.ts
@@ -1,26 +1,39 @@
-import { Request, Response, NextFunction } from "express";
 import Stripe from "stripe";
+import { Request, Response, NextFunction } from "express";
 
+import { User, UserStore } from "../models/UserStore";
 import { Service, ServiceStore } from "../models/ServiceStore";
 import { Payment, PaymentStore } from "../models/PaymentStore";
 import { Booking, BookingStore } from "../models/BookingStore";
+import BadRequestError from "../classes/base_errors/user_facing_errors/BadRequestError";
+import Logger from "../classes/logger/Logger";
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string, {
+const { STRIPE_SECRET_KEY, CLIENT_URL, STRIPE_ENDPOINT_SECRET } = process.env;
+
+const stripe = new Stripe(STRIPE_SECRET_KEY as string, {
   apiVersion: "2020-08-27"
 });
 
-const { CLIENT_URL } = process.env;
-
+const userStore = new UserStore();
 const serviceStore = new ServiceStore();
 const paymentStore = new PaymentStore();
 const bookingStore = new BookingStore();
 
-const createCheckoutSession = async (
+const logger = new Logger("stripe_logs.txt");
+
+export const createCheckoutSession = async (
   req: Request,
   res: Response,
   next: NextFunction
 ) => {
+  let user!: User;
   let service!: Service;
+
+  try {
+    user = await userStore.show(Number(req.body.user_id));
+  } catch (err) {
+    next(err);
+  }
 
   try {
     service = await serviceStore.show(Number(req.body.service_id));
@@ -29,11 +42,12 @@ const createCheckoutSession = async (
   }
 
   try {
-    const lineItem = {
+    const LINE_ITEM = {
       price_data: {
         currency: "gbp",
         product_data: {
-          name: service.title
+          name: service.title,
+          description: service.description
         },
         unit_amount: service.price
       },
@@ -45,7 +59,9 @@ const createCheckoutSession = async (
       cancel_url: `${CLIENT_URL}/cancel`,
       payment_method_types: ["card"],
       mode: "payment",
-      line_items: [lineItem],
+      line_items: [LINE_ITEM],
+      customer_email: user.email_address,
+      shipping_address_collection: { allowed_countries: ["GB"] },
       metadata: {
         user_id: req.body.user_id,
         service_id: req.body.service_id
@@ -61,7 +77,7 @@ const createCheckoutSession = async (
       !STRIPE_AMOUT_TOTAL ||
       !STRIPE_PAYMENT_STATUS
     ) {
-      throw new Error("Not enough detail to make a payment");
+      throw new BadRequestError("instance", "type", "title", "detail");
     }
 
     const payment: Payment = {
@@ -83,8 +99,41 @@ const createCheckoutSession = async (
 
     res.status(200).json({ id: session.id, url: session.url });
   } catch (err) {
+    logger.error(err);
+
     next(err);
   }
 };
 
-export default createCheckoutSession;
+export const updatePaymentStatus = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  try {
+    const PAYLOAD = req.body;
+    const STRIPE_SIGNATURE = req.headers["stripe-signature"];
+
+    if (!PAYLOAD || !STRIPE_SIGNATURE || STRIPE_ENDPOINT_SECRET) {
+      throw new BadRequestError("instance", "type", "title", "detail");
+    }
+
+    const event = stripe.webhooks.constructEvent(
+      PAYLOAD,
+      STRIPE_SIGNATURE,
+      STRIPE_ENDPOINT_SECRET as string
+    );
+
+    if (event.type === "checkout.session.completed") {
+      const session = event.data.object;
+      console.log(session);
+      // update payment status based on stripe_reference / payment intent
+    }
+
+    res.sendStatus(200);
+  } catch (err) {
+    logger.error(err);
+
+    next(err);
+  }
+};
--- src/routes/stripeRouter.ts
+++ src/routes/stripeRouter.ts
@@ -1,9 +1,18 @@
 import express, { Router } from "express";
 
-import createCheckoutSession from "../controllers/stripeController";
+import {
+  createCheckoutSession,
+  updatePaymentStatus
+} from "../controllers/stripeController";
 
 const stripeRouter: Router = express.Router();
 
 stripeRouter.post("/create-checkout-session", createCheckoutSession);
+
+stripeRouter.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  updatePaymentStatus
+);
 
 export default stripeRouter;
